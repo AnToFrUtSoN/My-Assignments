4d360e48492e56dae539d0d78b997ec8824f0e13
AnToFrUtSoN <132930137+AnToFrUtSoN@users.noreply.github.com>
2023-05-10 22:27:40 +0530

    Update Calculator.java

--- day3/Calculator.java
+++ day3/Calculator.java
@@ -1,5 +1,32 @@
 package week1.day3;
 
 public class Calculator {
+	public void additionTwoNumber(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+		}
+	
+	public void subractionTwoNumber(int e, int f) {
+		int d=e-f;
+		System.out.println(d);
+		}	
+	
+	public void multipleTwoNumbers(double i, double j) {
+		double g=i*j;
+		System.out.println(g);
 
+		}
+	public void divideTwoNumber(float x, float y) {
+		float z=x/y;
+		System.out.println(z);
+	}
+	
+	public static void main(String[] args) {
+		Calculator calc=new Calculator();
+		calc.additionTwoNumber(15, 15);
+		calc.subractionTwoNumber(20, 10);
+		calc.multipleTwoNumbers(6.5, 2.5);
+		calc.divideTwoNumber(9.6f , 3.5f);
+		
+	}
 }
